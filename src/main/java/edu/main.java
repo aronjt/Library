6f3a8b9512fc8867c9833f8f24c0294eb7fd3fdf
Tuzson <kecseyt@gmail.com>
2020-12-17 14:32:29 +0100

    connection ready

--- src/main/java/edu/main.java
+++ src/main/java/edu/main.java
@@ -1,6 +1,29 @@
 package edu;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+       main sql = new main();
+        Connection connection = sql.getConnection();
+        System.out.println("connection ready");
+
     }
+
+    public Connection getConnection() throws SQLException {
+        String url      = "jdbc:mysql://localhost:3306/library?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";   //database specific url.
+
+        Properties properties = new Properties( );
+        properties.put( "user", "root" );
+        properties.put( "password", "123456789" );
+
+        Connection connection =
+                DriverManager.getConnection(url, properties);
+        return connection;
+    }
+
 }
