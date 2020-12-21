60f3fb628b223cf617791db26b8c263fcb63140a
aronjt <jtelekiaron@gmail.com>
2020-12-21 10:58:56 +0100

    most popular and avg takeout time

--- src/main/java/edu/Library.java
+++ src/main/java/edu/Library.java
@@ -2,7 +2,6 @@ package edu;
 
 import java.lang.reflect.Member;
 import java.sql.*;
-import java.util.Date;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -16,11 +15,14 @@ public class Library {
     public static void main(String[] args) throws SQLException {
 
         Library sql = new Library();
-        //  Connection connection = sql.getConnection();
+      //  Connection connection = sql.getConnection();
         System.out.println("connection ready");
-       // sql.getFullInfo();
+      //  sql.getFullInfo();
       //  sql.getMemberInfo();
-        sql.getDailyLog();
+      //  sql.rentedBookData();
+      //  sql.getDailyLog();
+      //  sql.mostPopularBook();
+        sql.avgTakeOutTime();
     }
 
     public void getFullInfo() throws SQLException {
@@ -37,24 +39,23 @@ public class Library {
     public Connection getConnection() throws SQLException {
 
         String url = "jdbc:mysql://localhost:3306/library?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";   //database specific url.
-        Properties properties = new Properties();
-        properties.put("user", "root");
-        properties.put("password", "123456789");
+        Properties properties = new Properties( );
+        properties.put( "user", "root" );
+        properties.put( "password", "123456789" );
 
         return DriverManager.getConnection(url, properties);
     }
-
     public void getMemberInfo() throws SQLException {
-        System.out.println("Írja be a felhasználó azonisítót: ");
-        Scanner sc = new Scanner(System.in);
-        int memberID = sc.nextInt();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT Member.Name, Book.Title from Member join Log on Log.Member_idMember=Member.idMember join Stock on stock.idStock=Log.Stock_idStock join Book on Stock.Book_ISBN=Book.ISBN where ? =Member.idMember");
-        preparedStatement.setInt(1, memberID);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
+        System.out.println("Írja be a felhasználó azonosítót: ");
+        Scanner sc=new Scanner(System.in);
+        int memberID=sc.nextInt();
+        PreparedStatement preparedStatement=connection.prepareStatement("SELECT Member.Name, Book.Title from Member join Log on Log.Member_idMember=Member.idMember join Stock on stock.idStock=Log.Stock_idStock join Book on Stock.Book_ISBN=Book.ISBN where ? =Member.idMember");
+        preparedStatement.setInt(1,memberID);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        while(resultSet.next()){
             String name = resultSet.getString("Member.Name");
-            String bookTitle = resultSet.getString("Book.Title");
-            System.out.println("Név: " + name + " Köny: " + bookTitle);
+            String bookTitle=resultSet.getString("Book.Title");
+            System.out.println("Név: " + name+ " Köny: "+bookTitle);
         }
 
     }
@@ -74,7 +75,7 @@ public class Library {
             Date deadline= resultSet.getDate("Deadline");
             Date actualTakeBack= resultSet.getDate("ActualTakeback");
             int stockIDStock= resultSet.getInt("Stock_idStock");
-            System.out.println("LogID: "+idLog+" Felszhasználó azonisító: "+memberIDdMember+" Kikölcsönzés dátuma: "+taketout+" Kiadva: "+deadline+"-ig"+"Visszahozatal dátuma: "+actualTakeBack+" Cikkszám: "+ stockIDStock);
+            System.out.println("LogID: "+idLog+" Felszhasználó azonisító: "+memberIDdMember+" Kikölcsönzés dátuma: "+taketout+" Kiadva: "+deadline+"-ig"+" Visszahozatal dátuma: "+actualTakeBack+" Cikkszám: "+ stockIDStock);
         }
     }
 
@@ -100,17 +101,30 @@ public class Library {
         resultSet = preparedStatement.executeQuery();
         System.out.println(title + " című kötet kiadásának adatai: ");
         while (resultSet.next()){
-
             String bookTitle = resultSet.getString(1);
             String name = resultSet.getString(2);
             Date date = resultSet.getDate(3);
-
             System.out.println(bookTitle + " " + name + " " + date);
             System.out.println("-------------------------------------");
-
-
         }
-
     }
 
+    public void mostPopularBook() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT Title FROM Library.Book WHERE ISBN = (SELECT Stock.Book_ISBN FROM Stock JOIN Log ON Log.Stock_idStock = Stock.idStock GROUP BY Stock.Book_ISBN ORDER BY COUNT(*) DESC LIMIT 1)");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String bookTitle = resultSet.getString("Title");
+            System.out.println("A legtöbbször kikölcsönzött könyv: " + bookTitle);
+        }
+    }
+
+    public void avgTakeOutTime() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT Title, (SUM(Log.ActualTakeback) - SUM(Log.Takeout)) / COUNT(*) FROM Library.Book JOIN Stock ON Stock.Book_ISBN = Book.ISBN JOIN Log ON Log.Stock_idStock = Stock.idStock WHERE Log.ActualTakeback IS NOT NULL GROUP BY Stock.Book_ISBN");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String bookTitle = resultSet.getString(1);
+            double avgTime = Math.round(resultSet.getDouble(2) * 100.0) / 100.0;
+            System.out.println(bookTitle + " átlagos kölcsönzési ideje: " + avgTime + " nap.");
+        }
+    }
 }
